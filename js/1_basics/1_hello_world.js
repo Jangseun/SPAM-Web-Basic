d58a61c774c55d2153edf44abd7456dd12b8da95
JangSeung <0sunghee122@gmail.com>
2025-02-26 22:30:27 +0900

    .

--- js/1_basics/1_hello_world.js
+++ js/1_basics/1_hello_world.js
@@ -1,10 +1,8 @@
-console.log('hello world');
-console.log('hello', 'world') // ','를 쓰면 이어지면서 띄어쓰기가 됨
+console.log('hello world'); // 자동 개행
+console.log('hello', 'world'); // ','를 쓰면 이어지면서 띄어쓰기가 됨
 
-/*
--실행결과-
-
-hello world
-hello world aaa
-
-*/
+/**
+ * -실행결과-
+ * hello world
+ * hello world
+ */
